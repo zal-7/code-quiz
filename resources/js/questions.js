42bf3256490f96091d09ebd52fef0b0afdda59a3
Afzal Akhtar <afzalakhtar@Afzals-Air.lan>
2020-06-15 21:58:35 -0700

    final changes

--- resources/js/questions.js
+++ resources/js/questions.js
@@ -2,27 +2,27 @@
 var questions = [
     {
         title: "Commonly Used data types DO NOT include:",
-        choices: ["stings", "alerts", "booleans", "numbers"],
+        choices: ["Batman", "alerts", "variables", "numbers"],
         answer: "alerts"
     },
     {
         title: "The condition in an if / else statment is enclosed within _____.",
-        choices: ["parentheses", "quotes", "curly brackets", "square brackets"],
+        choices: ["parentheses", "quotes", "Superman", "square brackets"],
         answer: "parentheses"
     },
     {
         title: "What javascipt method can we use to select an html element?",
-        choices: ["document.queryselector()", "document.getElementChild", "document.getElementById", "Both 1 and 3"],
-        answer: "Both 1 and 3"
+        choices: ["document.queryselector()", "Aquaman", "document.getElementById", "Both #1 and #3"],
+        answer: "Both #1 and #3"
     },
     {
         title: "What html tag is NOT included in the HEAD tag?",
-        choices: ["link", "meta", "title", "header"],
+        choices: ["Green Lantern", "meta", "title", "header"],
         answer: "header"
     },
     {
         title: "What attribute is used in html to decorate content?",
-        choices: ["css", "class", "src", "style"],
+        choices: ["css", "class", "Megaman", "style"],
         answer: "style"
     }
 ]
